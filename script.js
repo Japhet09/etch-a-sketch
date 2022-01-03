a4bc17ae3b7ceb5de5cf87e8be18ad978d459340
Japhet09 <japhetalfred19@gmail.com>
2022-01-03 11:04:44 +0100

    added mousehover effect on new grid

--- script.js
+++ script.js
@@ -27,10 +27,14 @@ defaultGrid()
 
 const defaultButton =document.createElement('button')
 defaultButton.classList.add('button');
-defaultButton.textContent = 'DEFAULT'
+defaultButton.textContent = '16 X 16'
 main.prepend(defaultButton)
 
-defaultButton.addEventListener('click', defaultGrid)
+defaultButton.addEventListener('click',()=>{ 
+    defaultGrid();
+    allSquares();
+    changeColor();
+})
 
 const clearButton =document.createElement('button')
 clearButton.classList.add('button');
@@ -47,14 +51,19 @@ function clearGrid(){
 }
 const newGrid = document.createElement('button')
 newGrid.classList.add('button');
-newGrid.textContent = 'NEW GRID'
+newGrid.textContent = 'CHOOSE'
 main.prepend(newGrid)
+newGrid.addEventListener('click',()=>{
+    setNewGrid()
+    allSquares()
+    changeColor()
+})
 
 
 
-const setNewGrid = function(){
+function setNewGrid (){
    
-    let grid = prompt('Enter the number of  sqaures:');
+    let grid = prompt('Enter the number of  sqaures: NOT MORE THAN 64:');
     clearGrid()
     for(i=0; i<grid*grid;i++){
         const div = document.createElement('div')
@@ -66,32 +75,38 @@ const setNewGrid = function(){
         container.style.gridTemplateRows = `repeat(${grid},1fr)`
         container.style.gridTemplateColumns = `repeat(${grid},1fr)`
     
+
+        //* Everytime sqaures are created;select all sqaures and add event
+        allSquares()
+        changeColor()
+
     }
 
 }
 
-newGrid.addEventListener('click', setNewGrid)
+//Get all the sqaures everytime new squares are created
+let squares;
+function allSquares(){
+squares = document.querySelectorAll('.square')
+}
+allSquares()
 
-const squares = document.querySelectorAll('.square')
-
-/*
-squares.forEach(square=>{
-    square.addEventListener('mouseover',event=>{
-        event.target.style.backgroundColor='blue'
-    })
-
-})
-*/
-squares.forEach(square=>{
+function changeColor(){
+squares.forEach((square)=>{
     square.addEventListener('mouseover',event=>{
         let randomR = Math.floor(Math.random()*256)
         let randomG = Math.floor(Math.random()*256)
         let randomB = Math.floor(Math.random()*256)
         event.target.style.backgroundColor= `rgb(${randomR},${randomG},${randomB})`
+    
+
+
     })
+    
 
 })
 
-
+}
+changeColor()
 
 
